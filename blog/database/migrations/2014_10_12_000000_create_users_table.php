634574c8f85ac58257a419c3256d8a984bff68c7
milos996 <ivankovicmilos5@gmail.com>
2018-07-24 14:03:54 +0200

    #MIIV-6 dodana migracija users

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('first name');
+            $table->string('last name');
+            $table->string('company');
+            $table->string('country');
             $table->rememberToken();
             $table->timestamps();
         });
